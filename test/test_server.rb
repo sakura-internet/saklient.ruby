3b3aabaef8aadae754121ad52e8f4d28a7d968bc
townewgokgok <townewgokgok@gmail.com>
2014-07-10 16:14:02 +0900

    Refactored test_server

--- test/test_server.rb
+++ test/test_server.rb
@@ -5,85 +5,91 @@ require 'saclient/cloud/api'
 require 'date'
 require 'SecureRandom'
 
+
+
 class TestServer < MiniTest::Unit::TestCase
   
-  def load_config
+  
+  
+  def setup
+    
+    # load config file
     root = File.dirname(__dir__)
     test_ok_file = root + '/testok';
-    assert(File.exist?(test_ok_file), test_ok_file+" is not found (You must 'touch' this file to confirm that the tests can make your expenses)")
+    assert File.exist?(test_ok_file), test_ok_file+" is not found (You must 'touch' this file to confirm that the tests can make your expenses)"
     config_file = root + '/config.sh'
-    assert(File.exist?(config_file), test_ok_file+" is not found (Read README.md)")
-    config = {}
+    assert File.exist?(config_file), test_ok_file+" is not found (Read README.md)"
+    @config = {}
     fh = open(config_file)
     fh.each {|line|
       if /^\s*export\s+(\w+)\s*=\s*(.+?)\s*$/.match(line) then
         key = $1
         value = $2
-        config[key.to_sym] = value.gsub(/'([^']*)'|"([^"]*)"|\\(.)|(.)/) {|m|
+        @config[key.to_sym] = value.gsub(/'([^']*)'|"([^"]*)"|\\(.)|(.)/) {|m|
           $1 || $2 || $3 || $4
         }
       end
     }
     fh.close
-    assert(config[:SACLOUD_TOKEN], 'SACLOUD_TOKEN must be defined in config.sh')
-    assert(config[:SACLOUD_SECRET], 'SACLOUD_SECRET must be defined in config.sh')
-    #assert(config[:SACLOUD_ZONE], 'SACLOUD_ZONE must be defined in config.sh')
-    config
+    assert @config[:SACLOUD_TOKEN], 'SACLOUD_TOKEN must be defined in config.sh'
+    assert @config[:SACLOUD_SECRET], 'SACLOUD_SECRET must be defined in config.sh'
+    #assert @config[:SACLOUD_ZONE], 'SACLOUD_ZONE must be defined in config.sh'
+    
+    # authorize
+    @api = Saclient::Cloud::API::authorize(@config[:SACLOUD_TOKEN], @config[:SACLOUD_SECRET])
+    @api = @api.inZone(@config[:SACLOUD_ZONE]) if @config[:SACLOUD_ZONE]
+    assert_instance_of Saclient::Cloud::API, @api
+    
   end
   
-  def authorize
-    config = load_config
-    ret = Saclient::Cloud::API::authorize(config[:SACLOUD_TOKEN], config[:SACLOUD_SECRET])
-    ret = ret.inZone(config[:SACLOUD_ZONE]) if config[:SACLOUD_ZONE]
-    assert_instance_of(Saclient::Cloud::API, ret)
-    ret
-  end
   
-  def test_all
+  
+  def test_find
     
-    # test_find
-    api = authorize
-    servers = api.server.find
-    assert_instance_of(Array, servers)
-    assert(1 <= servers.length)
+    servers = @api.server.find
+    assert_instance_of Array, servers
+    assert 1 <= servers.length
     
-    # test_fields
     servers.each {|server|
-      assert_instance_of(Saclient::Cloud::Resource::Server, server)
-      assert_instance_of(Saclient::Cloud::Resource::ServerPlan, server.plan)
-      assert(1 <= server.plan.cpu)
-      assert(1 <= server.plan.memory_mib)
-      assert(1 <= server.plan.memory_gib)
-      assert(server.plan.memory_mib / server.plan.memory_gib == 1024)
-      assert_instance_of(Array, server.tags)
+      assert_instance_of Saclient::Cloud::Resource::Server, server
+      assert_instance_of Saclient::Cloud::Resource::ServerPlan, server.plan
+      assert 1 <= server.plan.cpu
+      assert 1 <= server.plan.memory_mib
+      assert 1 <= server.plan.memory_gib
+      assert server.plan.memory_mib / server.plan.memory_gib == 1024
+      assert_instance_of Array, server.tags
       server.tags.each {|tag|
-        assert_instance_of(String, tag)
+        assert_instance_of String, tag
       }
     }
     
-    # test_create
+  end
+  
+  
+  
+  def test_create
     name = '!ruby_minitest-' + DateTime.now.strftime('%Y%m%d_%H%M%S') + '-' + SecureRandom.uuid[0, 8]
     description = 'This instance was created by saclient.ruby minitest'
     tag = 'saclient-test'
     cpu = 1
     mem = 2
     #
-    server = api.server.create
-    assert_instance_of(Saclient::Cloud::Resource::Server, server)
+    server = @api.server.create
+    assert_instance_of Saclient::Cloud::Resource::Server, server
     server.name = name
     server.description = description
     server.tags = [tag]
-    server.plan = api.product.server.get_by_spec(cpu, mem)
+    server.plan = @api.product.server.get_by_spec(cpu, mem)
     server.save
     #
-    assert(1 <= server.id.to_i)
-    assert(server.name == name)
-    assert(server.description == description)
-    assert_instance_of(Array, server.tags)
-    assert(server.tags.length == 1)
-    assert(server.tags[0] == tag)
-    assert(server.plan.cpu == cpu)
-    assert(server.plan.memory_gib == mem)
+    assert 1 <= server.id.to_i
+    assert server.name == name
+    assert server.description == description
+    assert_instance_of Array, server.tags
+    assert server.tags.length == 1
+    assert server.tags[0] == tag
+    assert server.plan.cpu == cpu
+    assert server.plan.memory_gib == mem
     
     # test_boot
     server.boot
@@ -102,5 +108,14 @@ class TestServer < MiniTest::Unit::TestCase
     
     
   end
+  
+  
+  
+  def teardown
+    @config = nil
+    @api = nil
+  end
+  
+  
   
 end
